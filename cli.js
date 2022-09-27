a9a7181e3ec6b770ea94e1cc51e7dcd6230b27ab
niharcv <niharcv@unc.edu>
2022-09-26 23:26:17 -0400

    npm init & added support for -h flag

---
+++ cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// check for -h flag
+
+if (process.argv[2] == '-h') {
+    console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n    -h            Show this help message and exit.\n    -n, -s        Latitude: N positive; S negative.\n    -e, -w        Longitude: E positive; W negative.\n    -z            Time zone: uses tz.guess() from moment-timezone by default.\n    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n    -j            Echo pretty JSON from open-meteo API and exit.');
+    process.exit(0);
+}
+
